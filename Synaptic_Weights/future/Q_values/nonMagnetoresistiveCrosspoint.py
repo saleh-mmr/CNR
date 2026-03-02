cbf97c96a3546020f735ccac94c558182f0a20fd
Saleh Mir Mohammad Rezaei <salehmmrezaei@gmail.com>
2026-03-02 09:43:06 +0100

    start only cartepole in future->base

--- Synaptic_Weights/future/Q_values/nonMagnetoresistiveCrosspoint.py
+++
@@ -1,9 +0,0 @@
-from baseCrosspoint import BaseCrosspoint
-from crosspointState import CrosspointState
-
-class NonMagnetoresistiveCrosspoint(BaseCrosspoint):
-    """
-    Negative crosspoint.
-    """
-    def conductance_p(self, state: CrosspointState) -> float:
-        return self.gp(state)
