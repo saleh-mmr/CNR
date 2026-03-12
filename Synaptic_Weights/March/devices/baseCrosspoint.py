653a2c6aa2829347c1fb5c77287c88272df959f2
Saleh Mir Mohammad Rezaei <salehmmrezaei@gmail.com>
2026-03-12 18:06:43 +0100

    ready to implement

---
+++ Synaptic_Weights/March/devices/baseCrosspoint.py
@@ -0,0 +1,29 @@
+from crosspointParams import CrossPointParams
+from crosspointState import CrosspointState
+import numpy as np
+
+
+class BaseCrosspoint:
+    def __init__(self, params: CrossPointParams, state: CrosspointState):
+        self.params = params
+        self.state = state
+
+    #  Following Method Redraws Noise
+    def redraw_noise(self, sigma):
+        noise = float(np.random.normal(0.0, sigma)) if sigma > 0 else 0.0
+        return noise
+
+    # Following Method Changes State (increment X and redraw noise)
+    def update_state(self):
+        self.state.increment_index()
+        sigma = self.params.get_sigma()
+        noise = self.redraw_noise(sigma)
+        self.state.update_noise(noise)
+
+    # Following Method Compute noisy G_p for Current State **DOES NOT UPDATE STATE**
+    def calculate_conductance_p(self, state):
+        index, state_noise = state.get_state()
+        a = float(self.params.a)
+        b = float(self.params.b)
+        conductance_without_noise = float(a * np.log10(index) + b)
+        return float(conductance_without_noise + state_noise)
---
+++ Synaptic_Weights/March/devices/nonMagnetoresistiveCrosspoint.py
@@ -0,0 +1,9 @@
+from baseCrosspoint import BaseCrosspoint
+from crosspointState import CrosspointState
+
+class NonMagnetoresistiveCrosspoint(BaseCrosspoint):
+    """
+    Negative crosspoint.
+    """
+    def conductance_p(self, state: CrosspointState) -> float:
+        return self.calculate_conductance_p(state)
