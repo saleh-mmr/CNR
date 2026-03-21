7950c0e6b96b727693a823ebdbd5f20290bc3058
Saleh Mir Mohammad Rezaei <salehmmrezaei@gmail.com>
2026-03-21 21:59:21 +0100

    some bugs fixed

--- Synaptic_Weights/March/scripts/script.py
+++ Synaptic_Weights/March/scripts/script.py
@@ -10,7 +10,7 @@ from learning.trainer import Trainer
 hyperparams = {
     "discount_factor": 0.99,
     "batch_size": 100,
-    "max_episodes": 1200,
+    "max_episodes": 3000,
     "max_steps": 200,
     "epsilon_max": 1.0,
     "epsilon_min": 0.01,
