74a86747956e70478dd95ff0d8a6b91f68339673
Saleh Mir Mohammad Rezaei <salehmmrezaei@gmail.com>
2026-05-19 13:43:32 +0200

    run_2026-05-17_17-13-01: Episode: 9000, Steps: 100, CP_reward: 100.00, MC_reward: 100.00, Epsilon: 0.04

--- Synaptic_Weights/March/scripts/plot_weights_heatmap.py
+++ Synaptic_Weights/March/scripts/plot_weights_heatmap.py
@@ -9,8 +9,8 @@ plt.rcParams.update({'font.size': 16})
 
 # Paths to your models
 base_dir = os.path.dirname(os.path.abspath(__file__))
-folder = os.path.join(base_dir, "run_2026-05-11_10-02-28")
-step = 19635
+folder = os.path.join(base_dir, "run_2026-05-17_17-13-01")
+step = 330546
 path1 = os.path.join(folder, f"CP_best_model_{step}.pth")
 path2 = os.path.join(folder, f"MC_best_model_{step}.pth")
 
