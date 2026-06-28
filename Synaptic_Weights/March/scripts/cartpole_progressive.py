5f190ed17bb3e9159eeff284afb83ba0e652e1d9
Saleh Mir Mohammad Rezaei <salehmmrezaei@gmail.com>
2026-06-28 08:30:19 +0200

    run_2026-06-26_00-54-50 for 2H-20 is done

--- Synaptic_Weights/March/scripts/cartpole_progressive.py
+++ Synaptic_Weights/March/scripts/cartpole_progressive.py
@@ -114,7 +114,7 @@ def main():
     # ---------------------------------------------------------
     # Settings
     # ---------------------------------------------------------
-    folder_name = "run_2026-06-25_18-03-16"
+    folder_name = "run_2026-06-26_00-54-50"
     folder = SCRIPT_DIR / "three_problems" / folder_name
 
     num_tests = 200
